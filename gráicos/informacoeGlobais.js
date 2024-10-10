9ccceffa3427c8369c7d73151d96316f5e333dcd
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-10-10 11:46:56 +0000

    10 out

--- gráicos/informacoeGlobais.js
+++ gráicos/informacoeGlobais.js
@@ -6,7 +6,8 @@ async function visualizarinformacoesGlobais() {
     console.log(dados); 
     const paragrafo = document.createElement ('p')
     paragrafo.classList.add('graficos__texto')
-    paragrafo.innerHTML = 'você sabia que o mundo tem $(dados.total_pessoas_mundo_pesoa: de
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+    const conteiner = document 
  
 }
 visualizarinformacoesGlobais()
