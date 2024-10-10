593eaec5f5c0f614a5fdd4f95656bf96881fdabe
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-10-10 11:43:13 +0000

    a

--- gráicos/informacoeGlobais.js
+++ gráicos/informacoeGlobais.js
@@ -1,5 +1,12 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
-async function viualizarinformacoesGlobais() {
-    
+async function visualizarinformacoesGlobais() {
+    const res = await fetch (url)
+    const dados = await res.json() 
+    console.log(dados); 
+    const paragrafo = document.createElement ('p')
+    paragrafo.classList.add('graficos__texto')
+    paragrafo.innerHTML = 'você sabia que o mundo tem $(dados.total_pessoas_mundo_pesoa: de
+ 
 }
+visualizarinformacoesGlobais()
