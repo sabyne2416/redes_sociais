2a7f0d48e89f1929dae98460b2738d3c3e64da43
sabyne <105920674+sabyne2416@users.noreply.github.com>
2024-10-17 08:46:33 -0300

    17/10

--- gráicos/informacoeGlobais.js
+++ gráicos/informacoeGlobais.js
@@ -3,7 +3,10 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarinformacoesGlobais() {
     const res = await fetch (url)
     const dados = await res.json() 
-    console.log(dados); 
+   const pesoasConectadas = (dados.total_pessoas_conectadas/1e9)
+   const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+   const horas = pareInt (dados.tempo_medio)
+   const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo )* 100).toFixed(2);
     const paragrafo = document.createElement ('p')
     paragrafo.classList.add('graficos__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
